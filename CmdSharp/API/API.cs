cbfdd3d07d2b4733db6c060ced87cf2fd9a34242
Yaroslav Kibysh <yanet.prod@gmail.com>
2019-01-17 12:45:30 +0200

    Rebuild Architecture, Part 2
    Move API to main project

---
+++ CmdSharp/API/API.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace CmdSharp
+{
+    public class API
+    {
+        public delegate void WriteEventMethod(object text);
+        public static event WriteEventMethod WriteEvent;
+
+        public delegate void WriteLineEventMethod(object text);
+        public static event WriteLineEventMethod WriteLineEvent;
+
+        public static void Start()
+        {
+            Start(new string[] { });
+        }
+
+        public static void Start(string[] args)
+        {
+            if (WriteLineEvent == null || WriteEvent == null)
+                throw new NullReferenceException();
+
+            MethodInfo WriteLineMethod = null, WriteMethod = null;
+
+            foreach(var m in typeof(API).GetMethods())
+            {
+                if (m.Name == "WriteLine")
+                    WriteLineMethod = m;
+                else if (m.Name == "Write")
+                    WriteMethod = m;
+            }
+
+            GlobalVars.API = new APICore(new API(), WriteLineMethod, WriteMethod);
+            CoreMain.Init(args);
+        }
+        
+        internal static void WriteLine(object text) { WriteLineEvent(text); }
+        internal static void Write(object text) { WriteEvent(text); }
+    }
+}
---
+++ CmdSharp/API/Common.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CmdSharp.API
+{
+    public class Common
+    {
+        public static object Process(string Command, bool HandleExceptions = true)
+        {
+            return Cmd.Process(Command, HandleExceptions);
+        }
+
+        public static object ProcessNative(string Method, params object[] Arguments)
+        {
+            List<object> args = new List<object>();
+
+            foreach (var o in Arguments)
+                args.Add(o);
+
+            return Cmd.InvokeMethod(Method, args.ToArray());
+        }
+
+        public static void DebugText(string text, ConsoleColor color = ConsoleColor.DarkGray, bool print = true)
+        {
+            Debug.DebugText(text, color, print);
+        }
+
+        public static List<EVariable> Variables { get { return CmdSharp.Variables.VarList; } }
+
+        public static void VariableSet(string name, object value, List<string> options = null)
+        {
+            CmdSharp.Variables.Set(name, value, options);
+        }
+
+        public static void VariableRemove(string name, bool force = false)
+        {
+            CmdSharp.Variables.Remove(name, force);
+        }
+
+        public static object VariableGet(string name)
+        {
+            return CmdSharp.Variables.Get(name);
+        }
+
+        public static bool VariableGetBool(string name)
+        {
+            return CmdSharp.Variables.GetBool(name);
+        }
+    }
+}
