c25bef3c8223625db81a9d22f6219a92cbef4d86
Yaroslav Kibysh <yanet.prod@gmail.com>
2019-01-23 20:06:07 +0200

    Update System.Windows.Forms.cs

--- CmdSharp/Wrappers/System.Windows.Forms.cs
+++ CmdSharp/Wrappers/System.Windows.Forms.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace System.Windows.Forms
+namespace System.Windows.Forms : IDisposable
 {
     public class Control
     {
